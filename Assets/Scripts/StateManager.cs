4493fdcfd94da114d7bd55e565b7be84c82d947e
NowWeWake <nowwewake@gmail.com>
2020-06-30 07:38:43 +0200

    Dirty Statemanager.

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -1,26 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace SA
 {
     public class StateManager : MonoBehaviour
     {
-        
-        public State currentState;
+        [HideInInspector] public MovementData movementData;
+        [HideInInspector] public CollisionData collisionData;
+        [HideInInspector] public float delta;
+        [HideInInspector] public Transform mTransform;
+
+        public bool isGrounded;
 
         protected void OnStart()
         {
-
+            mTransform = transform;
+            movementData = new MovementData();
         }
         protected void OnUpdate()
         {
-
+            delta = Time.deltaTime;
         }
         protected void OnFixedUpdate()
         {
-
+            delta = Time.fixedDeltaTime;
         }
+
+        #region Pure State Manager
+        public State currentState;
 
         private void Start()
         {
@@ -44,5 +53,21 @@ namespace SA
                 currentState.FixedTick(this);
             }
         }
+        #endregion
     }
+
+ 
+
+    public class MovementData
+    {
+        public float moveMagnitude;
+        public Vector3 moveDirection;
+        public Quaternion playerRotation;
+    }
+
+    public class CollisionData
+    {
+
+    }
+
 }
